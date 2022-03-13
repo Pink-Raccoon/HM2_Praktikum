3eb9a90cc74acbbe273310cbea4a307dc3134f4a
Pink-Raccoon <schweglerasha@gmail.com>
2022-03-13 21:22:58 +0100

    aufg1

--- AshaSchwegler_Serie_03/AshaSchwegler_S3_Aufg1.py
+++ AshaSchwegler_Serie_03/AshaSchwegler_S3_Aufg1.py
@@ -21,6 +21,8 @@ Delta1, Delta2 = sp.symbols ('Delta1 Delta2')
 f1 = 20 - 18*x1 - 2*x2**2
 f2 = -4*x2 * (x1-x2**2)
 
+x0 = sp.Matrix([[1.1],[0.9]],dtype='float')
+
 g = sp.Matrix([(f1),(f2)])
 
 A = g.subs([(x1,1.1),(x2,0.9)])
@@ -36,8 +38,38 @@ Df0 = Df.subs([(x1,1.1),(x2,0.9)])
 
 Delt = sp.Matrix([Delta1, Delta2])
 
-X_one = sp.linsolve(Df0*Delt + f_x0, Delta1, Delta2)
+delt0 = sp.linsolve(Df0*Delt + f_x0, Delta1, Delta2)
 
 
 
 
+arr_delta0 = np.array([[delt0.args[0][0]],[delt0.args[0][1]]])
+
+x_one = x0 + arr_delta0
+
+A_1 = g.subs([(x1,0.996),(x2,1.026)],dtype='float')
+f_x1= A_1.evalf()
+
+fx1_matr=np.array([[f_x1]],dtype='float')
+
+normx1 = np.linalg.norm(fx1_matr)
+x1x0 = np.array([[x_one - x0]],dtype='float')
+normx1x0= np.linalg.norm(x1x0)
+
+
+Df1 = Df.subs([(x1,0.996),(x2,1.026)])
+delt1 = sp.linsolve(Df1*Delt + f_x1, Delta1, Delta2)
+arr_delt1 = np.array([[delt1.args[0][0]],[delt1.args[0][1]]])
+
+x_two = x0 + arr_delt1
+
+A_2 = g.subs([(x1,1.104),(x2,0.875)],dtype='float')
+f_x2 = A_2.evalf()
+
+fx2_matr = np.array([[f_x2]],dtype='float')
+
+normx2 = np.linalg.norm(fx2_matr)
+x2x1 = np.array([[x_two - x_one]],dtype='float')
+normx1x0= np.linalg.norm(x2x1)
+
+print(normx1x0)
