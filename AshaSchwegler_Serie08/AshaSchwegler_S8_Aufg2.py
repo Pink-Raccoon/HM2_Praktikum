fc517c6675fba96973f833080a04673125b82ea4
Pink-Raccoon <schweglerasha@gmail.com>
2022-05-02 09:46:16 +0200

    serie9

--- AshaSchwegler_Serie08/AshaSchwegler_S8_Aufg2.py
+++ AshaSchwegler_Serie08/AshaSchwegler_S8_Aufg2.py
@@ -16,55 +16,55 @@ def f(x,m):
 
 
 #a
-def sumRechteck(a,b,n):
+def sumRechteck(a,b,n,m):
     h = float((b-a)/n)
     result = 0
     for i in range (n):
        # print("range sumRechteck: ", i, '\n')
-        result += f(a+i*h+(h/2),10)
+        result += f(a+i*h+(h/2),m)
     result *= h
     return result
     
     
     
-print("Summierte Rechtecksregel: t= ", sumRechteck(20,5,5), '\n')
+print("Summierte Rechtecksregel: t= ", sumRechteck(20,5,5,10), '\n')
 
 
 #b
 
-def sumTrapez(a,b,n):
+def sumTrapez(a,b,n,m):
     h = float((b-a)/n)
     result = 0
     for i in range (1,n):
        # print("range sumTrapez: ", i, '\n')
-        result += f(a+i*h,10)
-    result += (f(a,10)+f(b,10))/2
+        result += f(a+i*h,m)
+    result += (f(a,m)+f(b,m))/2
     result *= h
     return result
 
 # print("f(a):", f(20,10), '\n')
 # print("f(b):", f(5,10), '\n')
-print("Summierte Trapezregel: t= ", sumTrapez(20,5,5), '\n')
+print("Summierte Trapezregel: t= ", sumTrapez(20,5,5,10), '\n')
 
 
 #c
 
-def sumSimpson(a,b,n):
+def sumSimpson(a,b,n,m):
     h = float((b-a)/n)
     result1 = 0
     result2 = 0
     for i in range(1,n):
        # print("range sumSimpson1: ", i, '\n')
-        result1 += f(a+i*h,10)
-    result1 += (1/2)*f(a,10)
+        result1 += f(a+i*h,m)
+    result1 += (1/2)*f(a,m)
         
     for i in range(1,n+1):
        # print("range sumSimpson2: ", i, '\n')
-        result2 += f((a+(i-1)*h+a+i*h)/2+f(b,10),10)
+        result2 += f((a+(i-1)*h+a+i*h)/2+f(b,m),m)
     result2 *= 2
     return (h/3)*(result1+result2)
 
-print("Summierte Simpsonregel: t= ", sumSimpson(20,5,5), '\n')    
+print("Summierte Simpsonregel: t= ", sumSimpson(20,5,5,10), '\n')    
 
 
 
--- AshaSchwegler_Serie09/AshaSchwegler_S9_Aufg3.py
+++ AshaSchwegler_Serie09/AshaSchwegler_S9_Aufg3.py
@@ -7,20 +7,37 @@ Created on Mon Apr 25 16:46:56 2022
 
 import numpy as np
 
-def sumTrapez(a,b,n):
-    print("1.Schritt: Berechne Tj0 mit Summierten Trapezregel", '\n')
-    print("--------------------------------------------------", '\n')
-    print("a = " + str(a), '\n')
-    print("b = " + str(b), '\n')
-    print("b = " + str(n), '\n')
-    h = float((b-a)/n)
-    result = 0
-    for i in range (1,n-1):
-        result += f(a+i*h,10)
-    result += (f(a,10)+f(b,10))/2
-    result *= h
-    return result
+def sumTrapez(f, a, b, n):
+    print("     Startwert a = " + str(a))
+    print("       Endwert b = " + str(b))
+    print("Anzahl Schritte n = " + str(n))
+    sum = 0
+    h = (b-a)/n
+    print("  Schrittweite h = (b - a) / n = " + str(h))
+    for i in range(1, n):
+        sum += f(a + i*h)
+    return h * ((f(a) + f(b))/2 + sum)
 
-def AshaSchwegler_S9_Aufg3(f,a,b,m):
-    n = 2**
-   
+def AshaSchwegler_S9_Aufg3(f, a, b, m):
+    print("1. Berechne die Tj0 mit der Trapezregel:")
+    print("----------------------------------------")
+    n = m + 1
+    T = np.zeros((n, n))
+    for j in range(n):
+        T[j,0] = sumTrapez(f, a, b, 2**j)
+        print("2. Berechne die Tjk aus den Tj0:")
+        print("--------------------------------")
+    for k in range(1,n):
+        for j in range(m-k,-1,-1):
+            T[j,k] = (4**k*T[j+1, k-1] - T[j,k-1]) / (4**k - 1)
+            print("T{}{} = (4^{} * T{}{} - T{}{}) / (4^{} - 1) = {}".format(j, k, k, j + 1, k - 1, j, k - 1, k, T[j, k]))
+    print('\nTij = \n', T)
+    return T[0,m]
+
+def f(x):
+    return np.cos(x**2)
+
+
+
+
+print(AshaSchwegler_S9_Aufg3(f, 0, np.pi, 4))
